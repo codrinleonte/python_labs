a4f41f24c20fb6a81c5dd6130eee6a13f43ab355
codrinleonte <codryn_14@yahoo.com>
2017-11-20 14:30:50 +0200

    added LabIII Ex1,Ex2,Ex3

---
+++ LabIV/Ex1.py
@@ -0,0 +1,22 @@
+import sys
+
+
+
+try:
+     a, b = int(sys.argv[1]), int(sys.argv[2])
+except IndexError:
+    print
+    "nu sunt doua numere"
+    sys.exit()
+except ValueError:
+    print("nu sunt numere")
+    sys.exit()
+
+
+print(a + b)
+print(a - b)
+print(a * b)
+try:
+    print (a / b)
+except ZeroDivisionError:
+    print("infinit")
